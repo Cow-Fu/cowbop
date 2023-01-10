c7648058909f2bc3206d4bc00f33bd2f0f7cf9f7
nathan <nathanj758@gmail.com>
2023-01-10 04:11:54 -0500

    started work on cowbop

---
+++ main.py
@@ -0,0 +1,67 @@
+import nextcord
+from nextcord.ext import commands
+from pytube import YouTube
+import pytube
+
+intents = nextcord.Intents.all()
+bot = commands.Bot(intents=intents)
+
+
+async def join_vc_from_interaction(interaction: nextcord.Interaction):
+    if not interaction.user.voice:
+        return False
+    await interaction.user.voice.channel.connect()
+    return True
+
+
+def download_video(url:str):
+    videos = YouTube(url)
+    video: pytube.Stream = videos.streams.filter(mime_type="audio/mp4").last()
+    try:
+        video.download(filename="file.mp4")
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+
+GUILD_IDS = [852764173292142593]
+@bot.slash_command(guild_ids=GUILD_IDS, description="change volume")
+async def volume(interaction: nextcord.Interaction, volume: int):
+    if not 0 < volume <= 100:
+        await interaction.send("Value must be between 1 and 100", delete_after=10)
+        return
+    
+    interaction.guild.voice_client.source.volume = volume / 100
+    await interaction.send(f"Volume has been set to {volume}%", delete_after=10)
+
+
+@bot.slash_command(guild_ids=GUILD_IDS, description="pauses")
+async def pause(interaction: nextcord.Interaction):
+    await interaction.guild.voice_client.pause()
+
+
+@bot.slash_command(guild_ids=GUILD_IDS, description="resume")
+async def resume(interaction: nextcord.Interaction):
+    await interaction.guild.voice_client.resume()
+
+@bot.slash_command(guild_ids=GUILD_IDS, description="play music")
+async def play(interaction: nextcord.Interaction, url: str):
+    if not await join_vc_from_interaction(interaction):
+        await interaction.send("You need to join a voice channel.", delete_after=10)
+        return
+    if not download_video(url):
+        await interaction.send("Error downloading video", delete_after=10)
+        return
+
+    thing = nextcord.PCMVolumeTransformer(nextcord.FFmpegPCMAudio("file.mp4", options="-vn"))
+    thing.volume = .05
+
+    interaction.guild.voice_client.play(thing)
+    # await interaction.response.send_message(f"ur url nerd: {url}")
+
+# rawr.streams.filter(mime_type="audio/mp4").last().download()
+
+bot.run("MTA2MTQ3MDQ2NDgwMDczMTIxOA.Gtc6pS.HqY-gJRqjagxCxnmL_bj2fECiJ1wrzux9kaMA8")
+
+
