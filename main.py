da90e8edae4961e54f211b6d0db9569c9061c663
nathan <nathanj758@gmail.com>
2023-01-19 21:58:33 -0500

    bot now adds MusicBotCog

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import nextcord
 from nextcord.ext import commands
+import MusicBotCog
 
 
 BOT_ID = 1061470464800731218
@@ -8,5 +9,5 @@ GUILD_IDS = [852764173292142593]
 intents = nextcord.Intents.all()
 bot = commands.Bot(intents=intents, default_guild_ids=GUILD_IDS)
 
-# bot.add_cog
+bot.add_cog(MusicBotCog(bot, BOT_ID))
 bot.run("MTA2MTQ3MDQ2NDgwMDczMTIxOA.Gtc6pS.HqY-gJRqjagxCxnmL_bj2fECiJ1wrzux9kaMA8")
