bb416b41415b683e069d2907ffb0f5b7b23a74b2
nathan <nathanj758@gmail.com>
2023-01-19 21:58:51 -0500

    added empty (for now) functions

--- MusicBotCog.py
+++ MusicBotCog.py
@@ -6,7 +6,32 @@ class MusicBotCog(commands.Cog):
     def __init__(self, bot: commands.Bot, bot_id: int):
         self.bot = bot
         self.bot_id = bot_id
+        self._volume = 5
 
     @nextcord.slash_command(description="Adds music to the queue")
     def play(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Pauses current song")
+    def pause(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Resumes current song")
+    def resume(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Stops playing music")
+    def stop(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Skips current song")
+    def skip(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Volume")
+    def volume(interaction: nextcord.Interaction):
+        pass
+
+    @nextcord.slash_command(description="Displays the current song queue")
+    def queue(interaction: nextcord.Interaction):
         pass
