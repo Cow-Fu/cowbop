4a160adc4504915c66228ce9178f1e0169ae2ee7
nathan <nathanj758@gmail.com>
2023-01-22 21:41:21 -0500

    add basic queue functions to QueueManager

--- QueueManager.py
+++ QueueManager.py
@@ -1,5 +1,15 @@
+import YouTubeVideoBuilder
 
 
 class QueueManager:
-    def __init__():
-        pass
+    def __init__(self):
+        self._queue = []
+
+    def pop(self):
+        return self.queue.pop()
+
+    def get(self, index=0):
+        return self.queue[index]
+
+    def add(self, song):
+        self.queue.append(song)
