9a66feacc1d4a95f53974b0144125f6c6ddf5800
nathan <nathanj758@gmail.com>
2023-01-24 19:15:58 -0500

    impl adding music and is_empty method

--- QueueManager.py
+++ QueueManager.py
@@ -8,8 +8,15 @@ class QueueManager:
     def pop(self):
         return self.queue.pop()
 
-    def get(self, index=0):
+    def get(self, index):
         return self.queue[index]
 
-    def add(self, song):
-        self.queue.append(song)
+    def get_next(self):
+        return self.get(0)
+
+    def add(self, interaction, url):
+        video = YoutubeVideoBuilder.build(interaction, url)
+        self.queue.append(video)
+
+    def is_empty(self):
+        return not bool(self._queue)
