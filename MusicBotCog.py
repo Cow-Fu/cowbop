acf8f132973c0eea7bd49eacc7410142f4b9f2d6
nathan <nathanj758@gmail.com>
2023-01-24 19:12:31 -0500

    add self to methonds because am stupid and start on adding play functionality

--- MusicBotCog.py
+++ MusicBotCog.py
@@ -1,37 +1,45 @@
 import nextcord
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
+from QueueManager import QueueManager
 
 
 class MusicBotCog(commands.Cog):
     def __init__(self, bot: commands.Bot, bot_id: int):
-        self.bot = bot
-        self.bot_id = bot_id
+        self._bot = bot
+        self._bot_id = bot_id
         self._volume = 5
+        self.q = QueueManager()
 
     @nextcord.slash_command(description="Adds music to the queue")
-    def play(interaction: nextcord.Interaction):
-        pass
+    def play(self, interaction: nextcord.Interaction, url: str):
+        self.q.add(url)
+        if not self.q.is_empty() and not self.song_loop.is_running():
+            self.song_loop.start()
 
     @nextcord.slash_command(description="Pauses current song")
-    def pause(interaction: nextcord.Interaction):
+    def pause(self, interaction: nextcord.Interaction):
         pass
 
     @nextcord.slash_command(description="Resumes current song")
-    def resume(interaction: nextcord.Interaction):
+    def resume(self, interaction: nextcord.Interaction):
         pass
 
     @nextcord.slash_command(description="Stops playing music")
-    def stop(interaction: nextcord.Interaction):
+    def stop(self, interaction: nextcord.Interaction):
         pass
 
     @nextcord.slash_command(description="Skips current song")
-    def skip(interaction: nextcord.Interaction):
+    def skip(self, interaction: nextcord.Interaction):
         pass
 
     @nextcord.slash_command(description="Volume")
-    def volume(interaction: nextcord.Interaction):
+    def volume(self, interaction: nextcord.Interaction):
         pass
 
     @nextcord.slash_command(description="Displays the current song queue")
-    def queue(interaction: nextcord.Interaction):
+    def queue(self, interaction: nextcord.Interaction):
+        pass
+
+    @tasks.loop(seconds=1)
+    def song_loop():
         pass
