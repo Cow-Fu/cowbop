cfbf3c082a701b1118755a1413e84402e0ae1351
nathan <nathanj758@gmail.com>
2023-01-31 18:32:34 -0500

    fix imports and get token from env

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import nextcord
 from nextcord.ext import commands
-import MusicBotCog
-
+from MusicBotCog import MusicBotCog
+import os
 
 BOT_ID = 1061470464800731218
 GUILD_IDS = [852764173292142593]
@@ -10,4 +10,4 @@ intents = nextcord.Intents.all()
 bot = commands.Bot(intents=intents, default_guild_ids=GUILD_IDS)
 
 bot.add_cog(MusicBotCog(bot, BOT_ID))
-bot.run("MTA2MTQ3MDQ2NDgwMDczMTIxOA.Gtc6pS.HqY-gJRqjagxCxnmL_bj2fECiJ1wrzux9kaMA8")
+bot.run(os.getenv('BOT_TOKEN'))
