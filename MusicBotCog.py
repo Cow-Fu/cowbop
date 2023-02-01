c4b527cddb77c235865362a3e056afb6476f6d56
nathan <nathanj758@gmail.com>
2023-02-01 02:47:07 -0500

    add ability to play the songs

--- MusicBotCog.py
+++ MusicBotCog.py
@@ -2,6 +2,7 @@ import nextcord
 from nextcord.ext import commands, tasks
 from QueueManager import QueueManager
 from YouTubeVideo import YouTubeVideo
+import pytube
 
 
 class MusicBotCog(commands.Cog):
@@ -13,8 +14,13 @@ class MusicBotCog(commands.Cog):
 
     @nextcord.slash_command(description="Adds music to the queue")
     async def play(self, interaction: nextcord.Interaction, url: str):
-        self._queue.add(url)
-        if not self._queue.is_empty() and not self.song_loop.is_running():
+        if not interaction.guild.voice_client:
+            if not interaction.user.voice:
+                interaction.send("You must first join a voice channel", ephemeral=True)
+                return
+            await interaction.user.voice.channel.connect()
+        self._queue.add(interaction, url)
+        if not self.song_loop.is_running():
             self.song_loop.start()
 
     @nextcord.slash_command(description="Pauses current song")
@@ -28,7 +34,6 @@ class MusicBotCog(commands.Cog):
         vc: nextcord.VoiceClient = interaction.guild.voice_client
         vc.resume()
         await interaction.send("Playback has been resumed", ephemeral=True, delete_after=10)
-        
 
     @nextcord.slash_command(description="Stops playing music")
     async def stop(self, interaction: nextcord.Interaction):
@@ -60,6 +65,44 @@ class MusicBotCog(commands.Cog):
             text += f"{video.video.title}\n"
         await interaction.send(text)
 
+    def _download_video(self, yt_video: YouTubeVideo):
+        video = yt_video.video
+        stream: pytube.Stream
+        try:
+            stream = video.streams.filter(mime_type="audio/mp4").last()
+        except Exception:
+            video.bypass_age_gate()
+            stream = video.streams.filter(mime_type="audio/mp4").last()
+        try:
+            stream.download(filename="file.mp4")
+        except Exception as e:
+            return False
+        return True
+
+    async def _play_music(self, video: YouTubeVideo):
+        inter = video.interaction
+        vc: nextcord.VoiceClient = inter.guild.voice_client
+        if not vc:
+            await inter.user.voice.channel.connect()
+        if not vc.is_connected():
+            if not video.interaction.user.voice:
+                await inter.send("You need to be connected to a voice channel!", ephemeral=True)
+                return
+            await inter.user.voice.channel.connect()
+        if not self._download_video(video):
+            await inter.send("Unable to download video")
+            return
+        audio = nextcord.PCMVolumeTransformer(nextcord.FFmpegPCMAudio("file.mp4", options="-vn"))
+        audio.volume = self._volume / 100
+        video.interaction.guild.voice_client.play(audio)
+
     @tasks.loop(seconds=1)
-    def song_loop():
-        pass
+    async def song_loop(self):
+        if self._queue.is_empty():
+            self.song_loop.stop()
+            return
+        item: YouTubeVideo
+        item = self._queue.pop(0)
+        await self._play_music(item)
+
+
