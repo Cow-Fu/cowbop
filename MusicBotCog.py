32e59a7b2e1db2d7b33412440ce6b9dce86c3a39
nathan <nathanj758@gmail.com>
2023-02-05 18:13:01 -0500

    added awaits because I'm stupid

--- MusicBotCog.py
+++ MusicBotCog.py
@@ -11,28 +11,28 @@ class MusicBotCog(Cog):
 
     @nextcord.slash_command(description="Adds music to the queue")
     async def play(self, interaction: nextcord.Interaction, url: str):
-        self.media_controller.play(interaction, url)
+        await self.media_controller.play(interaction, url)
 
     @nextcord.slash_command(description="Pauses current song")
     async def pause(self, interaction: nextcord.Interaction):
-        self.media_controller.pause(interaction)
+        await self.media_controller.pause(interaction)
 
     @nextcord.slash_command(description="Resumes current song")
     async def resume(self, interaction: nextcord.Interaction):
-        self.media_controller.resume(interaction)
+        await self.media_controller.resume(interaction)
 
     @nextcord.slash_command(description="Stops playing music")
     async def stop(self, interaction: nextcord.Interaction):
-        self.media_controller.stop(interaction)
+        await self.media_controller.stop(interaction)
 
     @nextcord.slash_command(description="Skips current song")
     async def skip(self, interaction: nextcord.Interaction):
-        self.media_controller.skip(interaction)
+        await self.media_controller.skip(interaction)
 
     @nextcord.slash_command(description="Volume")
     async def volume(self, interaction: nextcord.Interaction, volume: int):
-        self.media_controller.volume(interaction, volume)
+        await self.media_controller.volume(interaction, volume)
 
     @nextcord.slash_command(description="Displays the current song queue")
     async def queue(self, interaction: nextcord.Interaction):
-        self.media_controller.queue(interaction)
+        await self.media_controller.queue(interaction)
